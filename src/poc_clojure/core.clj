6a4ad1fb4bfd4a7a80a0bb44355d13f18523c353
matsrietdijk <matsrietdijk@gmail.com>
2018-10-30 17:31:48 +0100

    Add reload middleware to app

--- src/poc_clojure/core.clj
+++ src/poc_clojure/core.clj
@@ -1,6 +1,7 @@
 (ns poc-clojure.core
   (:gen-class)
-  (:require [org.httpkit.server :as server]))
+  (:require [org.httpkit.server :as server]
+            [ring.middleware.reload :as middleware]))
 
 (defn app
   [req]
@@ -12,5 +13,5 @@
   "Application entry point"
   [& args]
   (let [port 8080]
-    (server/run-server app {:port port})
+    (server/run-server (middleware/wrap-reload #'app) {:port port})
     (println (str "Running webserver at http://127.0.0.1:" port "/"))))
